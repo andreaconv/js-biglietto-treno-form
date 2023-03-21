63d170cbed1d842d0cb175e312722baee3d8dbba
andreaconv <andrea2convertini@gmail.com>
2023-03-21 18:47:32 +0100

    [add] visibilità al biglietto

--- JS/script.js
+++ JS/script.js
@@ -3,6 +3,11 @@ const prezzoKm = 0.21,
       scontUnder18 = 0.2,
       scontOver65 = 0.4;
 
+
+
+let discount = "Standard"
+
+
 // 1. seleziono il bottone - aggiungemdo l'evento del click a cui do una funzione: 
 document.querySelector("button").addEventListener("click", function(){
   // 2. creo const km che mi legge il valore (aggiungerndo ".value" alla fine) inserito nel input
@@ -13,26 +18,35 @@ document.querySelector("button").addEventListener("click", function(){
   //PREZZO COMPLETO
   let prezzocompleto = km * prezzoKm;
 
-  //CALCOLO EVENTUALE SCONTO IN BASE ALL'ETÀ esercizio 1
-  // if (age < 18) {
-    // prezzocompleto -= (prezzocompleto * 0.2);
-
-  // }else if (age >= 65) {
-    // prezzocompleto -= (prezzocompleto * 0.4);
-  // }
-
   //CALCOLO EVENTUALE SCONTO IN BASE ALL'ETÀ esercizio 2
-  const Minorenne = "< 18"
-  const Over65 = ">= 65"
+  const Minorenne = "< 18";
+  const Over65 = ">= 65";
+
 
   if (age === Minorenne) {
     prezzocompleto -= (prezzocompleto * scontUnder18);
+    discount = "Young"
   }else if (age === Over65){
     prezzocompleto -= (prezzocompleto * scontOver65);
+    discount = "Senior"
   }
+
+  const biglietto = document.querySelector(".print");
+  biglietto.classList.remove('hide');
+  
+  // STAMPA 
+  
+  const name = document.getElementById("name").value;
+  const passeggero = document.getElementById("passeggero");
+  passeggero.innerHTML = `${name}`;
+
+  document.getElementById("discount").innerHTML = `${discount}`;
+
+
 
 
   const price = document.getElementById('price');
+
   price.innerHTML = `
   &euro; ${prezzocompleto.toFixed(2)}
   `;
